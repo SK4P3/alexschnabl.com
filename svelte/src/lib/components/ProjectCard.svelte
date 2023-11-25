56e4c4158f361c668b01eb7c6d9640a25da3b2ab
Alex <alex.schnabl02@gmail.com>
2023-11-25 23:22:40 +0100

    improved snap scrolling on mobile with excluded zone

--- svelte/src/lib/components/ProjectCard.svelte
+++ svelte/src/lib/components/ProjectCard.svelte
@@ -2,7 +2,6 @@
     import {getWebpUrl, ImageQuality, type Project} from "$lib/common";
 
     export let project: Project;
-
 </script>
 
 <a class="w-full aspect-[16/9] bg-neutral-800 rounded-md hover:scale-[102%] transition-all shadow-md" href="/projects/{project.id}">
